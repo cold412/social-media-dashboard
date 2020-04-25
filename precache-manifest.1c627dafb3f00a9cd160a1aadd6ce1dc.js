b2deea2af35b9712d037c08f3cae4b5f6822b9b3
cold412 <cold412+gitlab@protonmail.com>
2020-04-25 23:13:21 +0200

    Updates

---
+++ precache-manifest.1c627dafb3f00a9cd160a1aadd6ce1dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697e622e4b14248eb6216db69383bc9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6172b1b6364867c640da",
+    "url": "/static/css/main.1bfbfe9e.chunk.css"
+  },
+  {
+    "revision": "ca8277efce3a8e135cb7",
+    "url": "/static/js/2.51d86c90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51d86c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6172b1b6364867c640da",
+    "url": "/static/js/main.efdd23ab.chunk.js"
+  },
+  {
+    "revision": "e75d660a1997be3cba22",
+    "url": "/static/js/runtime-main.c407ebb3.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
